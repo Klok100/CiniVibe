d4c98ef221f2e40818cb96c14cd8c3cdf897627b
klok100@icloud.com <Klok@101>
2020-02-10 11:28:42 -0600

    Basic Screens Commit 2/10/20

---
+++ app/src/main/java/com/example/cinivibe/MovieTheatersListViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.cinivibe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieTheatersListViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_theaters_list_view);
+    }
+}
