8c05b6d9e16863e09982838811fa8b77b9ac1c57
klok100@icloud.com <Klok@101>
2020-02-11 11:47:03 -0600

    Basic Navigation 2/11/20

--- app/src/main/java/com/example/cinivibe/IndividualMovieActivity.java
+++ app/src/main/java/com/example/cinivibe/IndividualMovieActivity.java
@@ -11,4 +11,5 @@ public class IndividualMovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_movie);
     }
+
 }
